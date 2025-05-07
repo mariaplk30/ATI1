d569309af7d758bddad2c1278f32f3453b38fbb4
Maria Paula <mariaplk30@gmail.com>
2025-05-06 20:56:46 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ fetch("./conf/configES.json")
         console.log("Configuración cargada:", config);
 
         document.querySelector(".titulo").innerHTML = `
-            ${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}
+            ${config.sitio[0]}<span>${config.sitio[1]}</span> ${config.sitio[2]}
         `;
         document.querySelector(".texto-centro").textContent = config.saludo + ", ";
         document.querySelector('input[type="text"]').placeholder = config.nombre + "...";
@@ -17,3 +17,28 @@ fetch("./conf/configES.json")
     .catch(error => {
         console.error("Error:", error);
     });
+
+fetch("./datos/index.json")
+    .then(response => {
+        if (!response.ok) throw new Error("Error al cargar estudiantes");
+        return response.json();
+    })
+    .then(perfiles => {
+        const lista = document.querySelector(".estudiantes");
+
+        perfiles.forEach(est => {
+            const li = document.createElement("li");
+            li.classList.add("estudiante-item");
+
+            li.innerHTML = `
+                <img src="${est.imagen}" alt="Foto de ${est.nombre}">
+                <p>${est.nombre}</p>
+            `;
+
+            lista.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error("Error al cargar estudiantes:", error);
+    });
+
